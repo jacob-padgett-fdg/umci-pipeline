38a32c70127820a4f7f7b5eabea92c76d52c5423
Nathan Saint Clair <nathansc@aquanate.com>
2016-02-05 15:11:32 -0800

    Fixing cancel button once again.

--- global/front_desk/dailylog/dailylog_cancel.php
+++ global/front_desk/dailylog/dailylog_cancel.php
@@ -4,7 +4,7 @@ require_once("global-auth.inc");
 require_once("querylib.inc");
 
 // check do any daily log entries already exist
-$sql = "SELECT COUNT(*) AS theCount FROM dialylog_entries WHERE dailylog_id = '".$dailylog_id."'";
+$sql = "SELECT COUNT(*) AS theCount FROM dailylog_entries WHERE dailylog_id = '".$dailylog_id."'";
 $result = mysql_query( $sql );
 $row = mysql_fetch_object( $result );
 $entry_count = $row->theCount;
@@ -12,7 +12,7 @@ $entry_count = $row->theCount;
 // if not, delete the dailylog record
 if ($entry_count == 0)
 {
-    $sql = "DELETE FROM dailylog WHERE dialylog_id = '".$dailylog_id."'";
+    $sql = "DELETE FROM dailylog WHERE dailylog_id = '".$dailylog_id."'";
     $result = mysql_query( $sql );
 }
 
